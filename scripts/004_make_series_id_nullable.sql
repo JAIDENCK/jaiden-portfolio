02b351ccbfd13a2ef39cf870ae153f68ae2c35c7
JAIDENCK <146530635+JAIDENCK@users.noreply.github.com>
2025-10-25 21:29:58 +0100

    the deletion.

--- scripts/004_make_series_id_nullable.sql
+++
@@ -1,6 +0,0 @@
--- Make series_id nullable in portfolio_images to allow unassigned photos
-ALTER TABLE portfolio_images
-ALTER COLUMN series_id DROP NOT NULL;
-
--- Add index for faster queries on unassigned photos
-CREATE INDEX idx_portfolio_images_series_id_null ON portfolio_images (series_id) WHERE series_id IS NULL;
